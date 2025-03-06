6ba5e00eda26c4407642ee12a266d090949d4e3b
mohdaamir11 <venomaamir@gmail.com>
2025-03-06 15:40:25 +0530

    second commit

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -1,5 +1,29 @@
 package api.endpoints;
 
+//Swagger URI: https://petstore.swagger.io
+//Create user (Post): https://petstore.swagger.io/v2/user
+//Get user (Get): https://petstore.swagger.io/v2/user/{username}
+//Update user (Put): https://petstore.swagger.io/v2/user/{username}
+//Delete user (Delete): https://petstore.swagger.io/v2/user/{username}
+
+
 public class Routes {
 
+	public static String base_url = "https://petstore.swagger.io/v2";
+	
+	//user module
+	
+	public static String post_url = base_url+"/v2/user";
+	public static String get_url = base_url+"/user/{username}";
+	public static String update_url = base_url+"/user/{username}";
+	public static String delete_url = base_url+"/user/{username}";
+	
+	// Store Module
+	
+	// code here for all store module url
+	
+	
+	// Pet Module
+	
+	// code here for all pet module url
 }
